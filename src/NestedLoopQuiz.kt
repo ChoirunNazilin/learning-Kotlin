397ef0dcf35fc52a6ff06d911f1370bb5a5a7519
Choirun Nazilin <cnazilin@gmail.com>
2017-10-23 03:37:45 +0700

    Update NestedLoopQuiz.kt

--- src/NestedLoopQuiz.kt
+++ src/NestedLoopQuiz.kt
@@ -1,7 +1,12 @@
 fun main(args: Array<String>) {
-    val a = 4
-    for(i in 1..a){
-        for(x in -1)
+    val a = 5
+    for (b in 1..a) {
+        for (c in 4 downTo b) { //spasi
+            print(" ")
+        }
+        for (d in 1..b) { //bintang
+            print("*")
+        }
+        println()
     }
-
 }
