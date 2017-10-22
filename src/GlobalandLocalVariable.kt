4f5e1e04cfa0ea58ffc7d6c40763650ae6215bbb
ChoirunNazilin <cnacilin@gmail.com>
2017-10-23 03:26:17 +0700

    Latihan Global dan local variable

---
+++ src/GlobalandLocalVariable.kt
@@ -0,0 +1,11 @@
+var fullname="Choirun Nazilin" //Global variable
+fun display():Unit{
+    println(fullname)
+    //print("name:"+name)
+}
+
+fun main(args: Array<String>) {
+    var name="Nazilin" //local var
+    println(fullname)
+    print("name:" + name)
+}
